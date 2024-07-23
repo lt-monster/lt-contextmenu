821c657be2ad6d037a61d8d493ce98f45b4cd768
1023785565 <1023785565@qq.com>
2024-07-23 22:16:03 +0800

    perf：修改测试代码；

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,50 @@
 <script setup lang="ts">
 import { LtContextmenu, type MenuGroupOption, type MenuOption, type MenuValue } from '.';
 import FileExport from '@/components/icons/FileExport.vue'
+import GoogleContextmenu from '@/components/tests/GoogleContextmenu.vue'
+import Toggle from '@/components/Toggle/index.vue'
 
-const ltContextmenu = ref()
+const ltContextmenu = ref<InstanceType<typeof GoogleContextmenu>>()
 
 const data = {
   name: 'lijiatu'
 }
 
-const menuOptions = ref<MenuGroupOption[]>([
-  {
-    group: 'group1',
-    options: [
-      {
-        id: '1',
-        icon: () => h(FileExport),
-        label: '菜单1',
-        children: [
-          {
-            id: '1-1',
-            icon: () => h(FileExport),
-            label: '菜单1-1',
-            handler: (menuParam?: any, menuValue?: MenuValue, itemOption?: MenuOption) => {
-              // menu click event
-            },
-          },
-          {
-            id: '1-2',
-            icon: () => h(FileExport),
-            label: '菜单1-2'
-          },
-          {
-            id: '1-3',
-            icon: () => h(FileExport),
-            label: '菜单1-3',
-            children: [
-              {
-                id: '1-3-1',
-                icon: () => h(FileExport),
-                label: '菜单1-3-111111111111111111'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        id: '2',
-        icon: () => h(FileExport),
-        label: '菜单2'
-      }
-    ]
-  },
-  {
-    group: '2',
-    options: [
-      {
-        id: '3',
-        icon: () => h(FileExport),
-        label: '排序方式',
-        type: 'radio',
-        value: 'name',
-        children: [
-          {
-            id: '3-1',
-            icon: () => h(FileExport),
-            label: '名称',
-            value: 'name',
-            handler: (menuParam: any, value?: MenuValue, itemOption?: MenuOption) => {
-
-            }
-          },
-          {
-            id: '3-2',
-            icon: () => h(FileExport),
-            label: '修改日期',
-            value: 'date',
-            handler: (menuParam: any, value?: MenuValue, itemOption?: MenuOption) => {
-
-            }
-          },
-          {
-            id: '3-3',
-            icon: () => h(FileExport),
-            label: '类型',
-            value: 'type',
-            handler: (menuParam: any, value?: MenuValue, itemOption?: MenuOption) => {
-
-            }
-          },
-          {
-            id: '3-4',
-            icon: () => h(FileExport),
-            label: '大小',
-            value: 'size',
-            handler: (menuParam: any, value?: MenuValue, itemOption?: MenuOption) => {
-
-            }
-          }
-        ]
-      },
-      {
-        id: '4',
-        icon: () => h(FileExport),
-        label: '菜单4',
-        disabled: true
-      }
-    ]
-  }
-])
-
 </script>
 
 <template>
-  <div style="width: 100%;height: 100%;" @click.right="(e) => ltContextmenu?.open(e, data)">
-    <LtContextmenu ref="ltContextmenu" :menu-options="menuOptions" :width="180" menu-size="small"
-      menu-theme="dark-element" />
+  <div style="display: flex;flex-direction: column;justify-content: center;align-items: center;">
+    <div style="width: 200px;height: 200px;background-color: palevioletred;"
+      @click.right="(e) => ltContextmenu?.open(e, data)">
+      <GoogleContextmenu ref="ltContextmenu" />
+    </div>
+    <Toggle />
+    <svg t="1721319046134" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
+      p-id="2726" width="200" height="200">
+      <path
+        d="M284.202667 258.005333a232.32 232.32 0 0 0-9.728 8.832 341.376 341.376 0 1 0 477.269333 2.133334 245.76 245.76 0 0 0-11.946667-10.88 45.653333 45.653333 0 0 1-7.552-60.714667 40.021333 40.021333 0 0 1 58.538667-7.68c10.624 8.917333 19.072 16.597333 25.429333 23.04A426.154667 426.154667 0 0 1 938.666667 512c0 235.648-191.018667 426.666667-426.666667 426.666667S85.333333 747.648 85.333333 512a426.197333 426.197333 0 0 1 147.2-322.346667 40.490667 40.490667 0 0 1 59.221334 7.722667 45.525333 45.525333 0 0 1-7.552 60.586667zM469.333333 469.333333V128a42.666667 42.666667 0 0 1 85.333334 0v341.333333a42.666667 42.666667 0 0 1-85.333334 0z"
+        fill="#5D6E7F" p-id="2727"></path>
+    </svg>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.toggle-main {
+  position: relative;
+  border: 1px solid pink;
+  border-radius: 9999px;
+  height: 1em;
+  width: 2em;
+  cursor: pointer;
+}
+
+.toggle-main>div {
+  width: .8em;
+  height: .8em;
+  background: pink;
+  border-radius: 9999px;
+  position: absolute;
+  top: .1em;
+  left: .1em;
+}
+</style>
--- src/components/tests/GoogleContextmenu.vue
+++ src/components/tests/GoogleContextmenu.vue
@@ -1,214 +1,108 @@
 <script setup lang="ts">
-import LtContextmenu from '../lt-contextmenu/LtContextmenu.vue'
-import type { MenuGroupOption, MenuOption, MenuValue } from '../lt-contextmenu/types/lt-contextmenu'
-import FileExport from '@/components/icons/FileExport.vue'
-
-type TestInfo = {
-    id: string
-    name: string
-    icon: string
-}
-
-const testInfo = ref<TestInfo>({
-    id: '1',
-    name: '菜单1',
-    icon: 'FileExport'
-})
+import LtContextmenu from "../lt-contextmenu/LtContextmenu.vue"
+import type { MenuGroupOption, MenuOption, MenuValue } from "../lt-contextmenu/types/lt-contextmenu"
+import FileExport from "@/components/icons/FileExport.vue"
+import IconSortMode from "@/components/icons/IconSortMode.vue"
+import IconSortAsc from "@/components/icons/IconSortAsc.vue"
+import IconSortDesc from "@/components/icons/IconSortDesc.vue"
+import IconSortNone from "@/components/icons/IconSortNone.vue"
+import IconDetail from "@/components/icons/IconDetail.vue"
+import IconRefresh from "@/components/icons/IconRefresh.vue"
+import IconAdd from "@/components/icons/IconAdd.vue"
+import IconModify from "@/components/icons/IconModify.vue"
+import IconDelete from "@/components/icons/IconDelete.vue"
+import IconCollect from "@/components/icons/IconCollect.vue"
+import IconToggle from "@/components/icons/IconToggle.vue"
 
 const tlContextmenuRef = ref<InstanceType<typeof LtContextmenu>>()
 
+const selectRadioValue = ref('none')
+
 const menuOptions = ref<MenuGroupOption[]>([
     {
-        group: 'group1',
+        group: "group1",
         options: [
             {
-                id: '1',
-                icon: () => h(FileExport),
-                label: '菜单11',
-                handler: (menuParam?: TestInfo, menuValue?: MenuValue, itemOption?: MenuOption) => {
-                    // console.log('menuParam', menuParam)
-                },
-                children: [
-                    {
-                        id: '1-1',
-                        icon: () => h(FileExport),
-                        label: '菜单1-1111111111111',
-                        handler: (menuParam?: TestInfo, menuValue?: MenuValue, itemOption?: MenuOption) => {
-                            console.log('menuParam', menuParam)
-                            console.log('menuValue', menuValue)
-                            console.log('itemOption', itemOption)
-                        },
-                    },
-                    {
-                        id: '1-2',
-                        icon: () => h(FileExport),
-                        label: '菜单1-2'
-                    },
-                    {
-                        id: '1-3',
-                        icon: () => h(FileExport),
-                        label: '菜单1-3',
-                        children: [
-                            {
-                                id: '1-3-1',
-                                icon: () => h(FileExport),
-                                label: '菜单1-3-111111111111111111'
-                            }
-                        ]
-                    }
-                ]
+                id: "1",
+                icon: () => h(IconRefresh),
+                label: "刷新"
             },
             {
-                id: '2',
-                // icon: () => h(FileExport),
-                label: '菜单2',
-                children: [
-                    {
-                        group: 'group2-1',
-                        options: [
-                            {
-                                id: '2-1',
-                                icon: () => h(FileExport),
-                                label: '菜单2-1'
-                            },
-                            {
-                                id: '2-2',
-                                icon: () => h(FileExport),
-                                label: '菜单2-2'
-                            }
-                        ]
-                    },
-                    {
-                        group: 'group2-2',
-                        options: [
-                            {
-                                id: '2-3',
-                                icon: () => h(FileExport),
-                                label: '菜单2-3'
-                            },
-                            {
-                                id: '2-4',
-                                icon: () => h(FileExport),
-                                label: '菜单2-4'
-                            }
-                        ]
-                    }
-                ]
-            }
+                id: "2",
+                icon: () => h(IconDetail),
+                label: "详情",
+                disabled: true
+            },
         ]
     },
     {
-        group: '2',
+        group: "2",
         options: [
             {
-                id: '3',
-                icon: () => h(FileExport),
-                label: '排序方式',
+                id: "2-2",
+                icon: () => h(IconAdd),
+                label: "添加"
+            },
+            {
+                id: "2-1",
+                icon: () => h(IconSortMode),
+                label: "排序方式",
                 type: 'radio',
-                value: 'name',
+                value: 'none',
+                change(menuParam, value, itemOption) {
+                    console.log('menuParam', menuParam)
+                    console.log('value', value)
+                    console.log('itemOption', itemOption)
+                },
                 children: [
                     {
-                        id: '3-1',
-                        icon: () => h(FileExport),
-                        label: '名称',
-                        value: 'name',
-                        handler: (menuParam: TestInfo, value?: MenuValue, itemOption?: MenuOption) => {
-                            // console.log('value', value)
-                            // console.log('itemOption', itemOption)
+                        id: "2-1-1",
+                        icon: () => h(IconSortAsc),
+                        label: "升序",
+                        value: 'asc',
+                        handler: (menuParam?: any, value?: MenuValue, itemOption?: MenuOption) => {
+                            selectRadioValue.value = '升序'
                         }
                     },
                     {
-                        id: '3-2',
-                        icon: () => h(FileExport),
-                        label: '修改日期',
-                        value: 'date',
-                        handler: (menuParam: TestInfo, value?: MenuValue, itemOption?: MenuOption) => {
-                            // console.log('value', value)
-                            // console.log('itemOption', itemOption)
+                        id: "2-1-2",
+                        icon: () => h(IconSortDesc),
+                        label: "降序",
+                        value: 'desc',
+                        handler: (menuParam?: any, value?: MenuValue, itemOption?: MenuOption) => {
+                            selectRadioValue.value = '降序'
                         }
                     },
                     {
-                        id: '3-3',
-                        icon: () => h(FileExport),
-                        label: '类型',
-                        value: 'type',
-                        handler: (menuParam: TestInfo, value?: MenuValue, itemOption?: MenuOption) => {
-                            // console.log('value', value)
-                            // console.log('itemOption', itemOption)
+                        id: "2-1-3",
+                        icon: () => h(IconSortNone),
+                        label: "不排序",
+                        value: 'none',
+                        handler: (menuParam?: any, value?: MenuValue, itemOption?: MenuOption) => {
+                            selectRadioValue.value = '不排序'
                         }
                     },
-                    {
-                        id: '3-4',
-                        icon: () => h(FileExport),
-                        label: '大小',
-                        value: 'size',
-                        handler: (menuParam: TestInfo, value?: MenuValue, itemOption?: MenuOption) => {
-                            // console.log('value', value)
-                            // console.log('itemOption', itemOption)
-                        }
-                    }
                 ]
             },
             {
-                id: '4',
-                icon: () => h(FileExport),
-                label: '菜单4',
-                disabled: true
-            },
-            {
-                id: '5',
-                icon: () => h(FileExport),
-                label: '重新加载111111111111',
-                visible: (menuParam: TestInfo) => {
-                    return false
-                }
-            },
-            {
-                id: '6',
-                icon: () => h(FileExport),
-                label: '多选',
-                type: 'checkbox',
-                value: ['name', 'date'],
-                children: [
-                    {
-                        id: '6-1',
-                        icon: () => h(FileExport),
-                        label: '名称',
-                        value: 'name',
-                        handler: (menuParam: TestInfo, value?: MenuValue) => {
-                            console.log('value', value)
-                        }
-                    },
-                    {
-                        id: '6-2',
-                        icon: () => h(FileExport),
-                        label: '修改日期',
-                        value: 'date',
-                        handler: (menuParam: TestInfo, value?: MenuValue) => {
-                            console.log('value', value)
-                        }
-                    },
-                    {
-                        id: '6-3',
-                        icon: () => h(FileExport),
-                        label: '类型',
-                        value: 'type',
-                        handler: (menuParam: TestInfo, value?: MenuValue) => {
-                            console.log('value', value)
-                        }
-                    }
-                ]
-            },
-            {
-                id: '7',
-                label: '类型',
-                disabled(menuParam, itemOption) {
-                    return true
+                id: "2-3",
+                icon: () => h(IconToggle),
+                label: "开关",
+                type: 'toggle',
+                value: false,
+                change(menuParam, value, itemOption) {
+                    console.log('value', value)
                 },
             }
         ]
     }
 ])
+
+function customClose(close: () => void) {
+    console.log('关闭前')
+    close()
+    console.log('关闭后')
+}
 
 defineExpose({
     open: (event: MouseEvent, param?: any) => tlContextmenuRef.value?.open(event, param),
@@ -220,20 +114,8 @@ defineExpose({
 </script>
 
 <template>
-    <LtContextmenu ref="tlContextmenuRef" :menu-options="menuOptions" menu-style="google" menu-theme="light"
-        menu-size="normal" :width="240" :max-width="300" :group-class="() => 'aaaa'" :group-style="{ color: '#606266' }"
-        :item-class="'bbbb'" :item-style="{ color: '#606266' }">
-        <!-- <template #header="{ menuParam }">
-            <div>
-                我是头部,{{ menuParam.name }}
-            </div>
-        </template> -->
-        <!-- <template #footer="{ menuParam }">
-            <div>
-                我是底部,{{ menuParam.name }}
-            </div>
-        </template> -->
-    </LtContextmenu>
+    <LtContextmenu ref="tlContextmenuRef" :menu-options="menuOptions" menu-size="small" :width="180"
+        :before-close="customClose" />
 </template>
 
 <style scoped></style>
